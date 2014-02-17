c14d6c2034aba5b8b080d417e6e7de0eb2761268
Koop Otten <koop@noorderstorm.com>
2014-02-17 14:08:56 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstWeekApp::Application.config.secret_key_base = 'f0198a4a2b97bca9eff1877594445f15d477640bed5f9f98c0a76879df4e594502d9f51db4860151a4f8a18b803cc964cc84ff9169edab5d85959d79daaffbe8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstWeekApp::Application.config.session_store :cookie_store, key: '_FirstWeekApp_session'
