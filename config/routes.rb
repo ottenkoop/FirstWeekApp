7b90222cb228205a369c846c14367629c5edc05a
Nick de Vries <nick.vries@gmail.com>
2014-02-18 13:11:55 +0100

    Add resources user stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FirstWeekApp::Application.routes.draw do
+
+  resources :userstories
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
