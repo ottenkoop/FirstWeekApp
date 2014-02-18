28f64de7b1f8260f93a5fa1b1d24f03ad13ee946
Nick de Vries <nick.vries@gmail.com>
2014-02-18 13:10:26 +0100

    user stories controller + new.html.erb

--- app/controllers/userstories_controller.rb
+++ app/controllers/userstories_controller.rb
@@ -1,2 +1,21 @@
-class UserstoriesControllerController < ApplicationController
+class UserstoriesController < ApplicationController
+
+def index
+	@userstories = Userstories.all
+end
+
+def new
+	@userstory = Userstories.new
+end
+
+def create
+	@userstory = Userstories.new(userstories_params)
+	@userstory.save
+end
+
+private
+
+def userstories_params
+	params.require(:userstories).permit(:content, :rating, :sprint_id)
+end
 end
