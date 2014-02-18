2aae6c2f25862376bda50a2427ff723c529b69df
Koop Otten <koop@noorderstorm.com>
2014-02-18 12:57:13 +0100

    Add index, new and create function

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -1,2 +1,25 @@
-class SprintsControllerController < ApplicationController
+class SprintsController < ApplicationController
+
+  def index
+    @sprints = Sprint.all
+  end
+
+  def new
+    @sprint = Sprint.new
+  end
+
+  def create
+    @sprint = Sprint.new(sprintparams)
+
+    if @sprint.save
+      redirect_to root_path, notice: "New Sprint Added"
+    else
+      render 'new', alert: "Someting went wrong"
+  end
+
+  private
+
+  def sprintparams
+    params.require(:sprint).permit(:name, :goal, :start_date, :end_date)
+  end
 end
