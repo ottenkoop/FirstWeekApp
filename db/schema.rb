e19af2db287a828fc8aa75652545180d7ea56e23
Koop Otten <koop@noorderstorm.com>
2014-02-18 12:10:38 +0100

    Change sqlite to PG

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140218104108) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "sprints", force: true do |t|
     t.string   "name"
     t.datetime "start_date"
